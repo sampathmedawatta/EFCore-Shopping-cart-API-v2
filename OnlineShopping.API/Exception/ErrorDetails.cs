7b265212f3315084340dac96bc698724cbd46f28
SamVPS <sampath.m@virtualplanet.com.au>
2020-07-15 12:35:13 +0530

    serialise tostring

--- OnlineShopping.API/Exception/ErrorDetails.cs
+++ OnlineShopping.API/Exception/ErrorDetails.cs
@@ -1,8 +1,15 @@
-﻿namespace OnlineShopping.API.Exception
+﻿using Newtonsoft.Json;
+
+namespace OnlineShopping.API.Exception
 {
     public class ErrorDetails
     {
         public int StatusCode { get; set; }
         public string Message { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
     }
 }
